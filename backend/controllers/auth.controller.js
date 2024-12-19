23a02abb2083321dade3142049f08b78c476ddba
Dilan Pramodhya <dilanpramodhya44@gmail.com>
2024-12-19 13:47:08 +0530

    User Authenticate part (generateToken, accessToken) done

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,40 @@
+import { redis } from "../lib/redis.js";
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+const generateToken = (userId) => {
+  const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "15m",
+  });
+  const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
+  return { accessToken, refreshToken };
+};
+
+const storeRefreshToken = async (userId, refreshToken) => {
+  await redis.set(
+    `refresh_token: ${userId}`,
+    refreshToken,
+    "EX",
+    7 * 24 * 60 * 60 // 7 days
+  );
+};
+
+const setCookies = (res, accessToken, refreshToken) => {
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 15 * 60 * 100, // 15 minutes
+  });
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 100, // 7 minutes
+  });
+};
 
 export const SignUp = async (req, res) => {
   const { name, email, password } = req.body;
@@ -15,7 +51,22 @@ export const SignUp = async (req, res) => {
 
     const user = await User.create({ name, email, password });
 
-    res.status(201).json({ user, message: "User Created Successfully" });
+    // Authenticate User
+    const { accessToken, refreshToken } = generateToken(user._id);
+
+    await storeRefreshToken(user._id, refreshToken);
+
+    setCookies(res, accessToken, refreshToken);
+
+    res.status(201).json({
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+      message: "User Created Successfully",
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }

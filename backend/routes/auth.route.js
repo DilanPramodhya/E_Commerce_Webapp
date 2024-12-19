c71bb41012710eb737111c53b672f3b0f0292338
Dilan Pramodhya <dilanpramodhya44@gmail.com>
2024-12-19 16:00:29 +0530

    Refresh token done

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { Login, Logout, SignUp } from "../controllers/auth.controller.js";
+import { Login, Logout, RefreshToken, SignUp } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signUp", SignUp);
 router.post("/login", Login);
 router.post("/logout", Logout);
+router.post("/refreshToken", RefreshToken);
 
 export default router;

a987b1189d55d5c6eadc6806ff3370cb717d362f
Dilan Pramodhya <dilanpramodhya44@gmail.com>
2024-12-19 22:43:29 +0530

    GetAllProducts, GetFeaturedProducts done

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,3 +1,4 @@
+import { redis } from "../lib/redis.js";
 import Product from "../models/product.model.js";
 
 export const GetAllProducts = async (req, res) => {
@@ -6,6 +7,29 @@ export const GetAllProducts = async (req, res) => {
     res.json({ products });
   } catch (error) {
     console.log("Error in getall products", error.message);
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+export const GetFeaturedProducts = async (req, res) => {
+  try {
+    let featuredProducts = await redis.get("featured_products");
+    if (featuredProducts) {
+      return res.json(JSON.parse(featuredProducts));
+    }
+
+    // if not in redis, fetch from MONGODB
+    featuredProducts = await Product.find({ isFeatured: true }).lean(); // .lean() is gonna a return a plain javascript object instead of a mongodb document
+
+    if (!featuredProducts) {
+      return res.status(404).json({ message: "No featured product found" });
+    }
+
+    // Store in redis for future quick access
+    await redis.set("featured_products", JSON.stringify(featuredProducts));
+
+    res.json(featuredProducts);
+  } catch (error) {
+    console.log("Error in getFeatured products", error.message);
     res.status(500).json({ message: "Server Error", error: error.message });
   }
 };
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -3,14 +3,16 @@ import {
   CreateProduct,
   DeleteProduct,
   GetAllProducts,
+  GetFeaturedProducts,
   UpdateProduct,
 } from "../controllers/product.controller.js";
 import { AdminRoute, ProtectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/create", CreateProduct);
 router.get("/", ProtectRoute, AdminRoute, GetAllProducts);
+router.get("/featured",  GetFeaturedProducts);
+router.get("/", ProtectRoute, AdminRoute, CreateProduct);
 router.put("/update/:id", UpdateProduct);
 router.post("/delete/:id", DeleteProduct);
 // router.get("/getProfile", GetProfile);

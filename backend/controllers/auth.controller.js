c5cad2fd830d6403902adf1fa5068d2b7aa18086
Dilan Pramodhya <dilanpramodhya44@gmail.com>
2024-12-19 12:04:38 +0530

    SignUp and Hashed password done

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,24 @@
+import User from "../models/user.model.js";
+
 export const SignUp = async (req, res) => {
-  res.send("Sign up router");
+  const { name, email, password } = req.body;
+
+  try {
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "Fill full form" });
+    }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "user Already Exists" });
+    }
+
+    const user = await User.create({ name, email, password });
+
+    res.status(201).json({ user, message: "User Created Successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const Login = async (req, res) => {
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -3,8 +3,8 @@ import { Login, Logout, SignUp } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/signUp", SignUp);
-router.get("/login", Login);
-router.get("/logout", Logout);
+router.post("/signUp", SignUp);
+router.post("/login", Login);
+router.post("/logout", Logout);
 
 export default router;
